e120700c63e9f5bcdf6ea46a1d8f526db2f72c41
Yangqun123456 <2537148609@qq.com>
2023-03-19 17:25:33 +0800

    review

--- static/js/index.js
+++ static/js/index.js
@@ -20,13 +20,13 @@ function alertmess(mess) {
 signIn.onclick = () => {
     const username = $('#username2').val()
     const password = $('#password2').val()
-    $.post('http://139.196.139.148:4002/api/login/', {
+    $.post('http://127.0.0.1:4002/api/login/', {
         username: username,
         password: password,
     }, async function (data) {
         if (data.status == 0) {
             await alertmess('登陆成功')
-            location.href = 'http://139.196.139.148:4002/mainpage/?username=' + username
+            location.href = 'http://127.0.0.1:4002/mainpage/?username=' + username
         } else alert(data.message)
     })
     return false;
@@ -36,14 +36,14 @@ signUp.onclick = () => {
     const username = $('#username1').val();
     const password = $('#password1').val();
     const email = $('#email1').val();
-    $.post('http://139.196.139.148:4002/api/reguser/', {
+    $.post('http://127.0.0.1:4002/api/reguser/', {
         username: username,
         password: password,
         email: email,
     }, function (data) {
         if (data.status == 0) {
             alertmess('注册成功')
-            location.href = 'http://139.196.139.148:4002/mainpage/?username=' + username
+            location.href = 'http://127.0.0.1:4002/mainpage/?username=' + username
         } else alert(data.message)
     })
     return false;
