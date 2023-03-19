c5aaaf3a65a4fb74482e932dda7fd5eb0af50d10
Yangqun123456 <2537148609@qq.com>
2023-03-19 16:29:45 +0800

    3.19 submit

--- static/js/index.js
+++ static/js/index.js
@@ -20,14 +20,14 @@ function alertmess(mess) {
 signIn.onclick = () => {
     const username = $('#username2').val()
     const password = $('#password2').val()
-    $.post('http://127.0.0.1:4002/api/login/', {
+    $.post('http://139.196.139.148:4002/api/login/', {
         username: username,
         password: password,
     }, async function (data) {
         data = JSON.parse(data)
         if (data.status == 0) {
             await alertmess('登陆成功')
-            location.href = 'http://localhost:4002/mainpage/?username=' + username
+            location.href = 'http://139.196.139.148:4002/mainpage/?username=' + username
         } else alert(data.message)
     })
     return false;
@@ -37,7 +37,7 @@ signUp.onclick = () => {
     const username = $('#username1').val();
     const password = $('#password1').val();
     const email = $('#email1').val();
-    $.post('http://127.0.0.1:4002/api/reguser/', {
+    $.post('http://139.196.139.148:4002/api/reguser/', {
         username: username,
         password: password,
         email: email,
@@ -45,7 +45,7 @@ signUp.onclick = () => {
         data = JSON.parse(data)
         if (data.status == 0) {
             alertmess('注册成功')
-            location.href = 'http://localhost:4002/mainpage/?username=' + username
+            location.href = 'http://139.196.139.148:4002/mainpage/?username=' + username
         } else alert(data.message)
     })
     return false;
